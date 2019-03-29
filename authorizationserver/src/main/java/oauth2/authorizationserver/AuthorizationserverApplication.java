0ebf6122e88e3e7470dec60aceaaab052eef7db3
andy_zhaozhao@hotmail.com <andy_zhaozhao@hotmail.com>
2019-03-29 22:21:00 +0800

    Add： init auhtoiaztion/resource server

---
+++ authorizationserver/src/main/java/oauth2/authorizationserver/AuthorizationserverApplication.java
@@ -0,0 +1,13 @@
+package oauth2.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationserverApplication.class, args);
+    }
+
+}
---
+++ resourceserver/src/main/java/oauth2/resourceserver/ResourceserverApplication.java
@@ -0,0 +1,13 @@
+package oauth2.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceserverApplication.class, args);
+    }
+
+}
